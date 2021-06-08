7380ca98d7597e2fc6894c0dbfc0cfd0e3bdd4d4
Florian Hitz <web.florianhitz@gmail.com>
2021-06-08 18:20:15 +0200

    Intial commit of zuckerschlaeck.ch

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>about</h1>
+    <a @click="$router.push('/')">Home</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>home</h1>
+    <a @click="$router.push('/about')">About</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
